cbd680a9c6a43249c204e140ffda76d3513dfe07
ihaveint <ihaveint@gmail.com>
2020-02-09 00:56:39 +0330

    Add resources

---
+++ src/Resources/SalonResource.cs
@@ -0,0 +1,9 @@
+namespace Booking.Resources
+{
+    public class SalonResource
+    {
+        public string Name { get; set; }
+        public int SeatWidth { get; set; }
+        public int SeatHeight { get; set; }
+    }
+}
---
+++ src/Resources/SaveSalonResource.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Booking.Resources
+{
+    public class SaveSalonResource
+    {
+        
+        [Required]
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        
+        [Required]
+        public int SeatWidth { get; set; }
+        
+        [Required]
+        public int SeatHeight { get; set; }
+    }
+}
