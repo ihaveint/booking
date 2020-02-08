14a0d0fbf2cde60e22dbfd51119404d953c619fb
ihaveint <ihaveint@gmail.com>
2020-02-09 02:49:15 +0330

    Add missing field to Salon resource

--- src/Resources/SalonResource.cs
+++ src/Resources/SalonResource.cs
@@ -2,6 +2,7 @@ namespace Booking.Resources
 {
     public class SalonResource
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public int SeatWidth { get; set; }
         public int SeatHeight { get; set; }
