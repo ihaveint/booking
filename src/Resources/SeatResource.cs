5f2bce49528aca5cdd49a0b16af630aee587de2d
ihaveint <ihaveint@gmail.com>
2020-02-09 02:53:41 +0330

    Add missing property to SeatResource.cs

--- src/Resources/SeatResource.cs
+++ src/Resources/SeatResource.cs
@@ -3,6 +3,7 @@ namespace Booking.Resources
     public class SeatResource
     {
         public int salonId { get; set; }
+        public int Id { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
     }
